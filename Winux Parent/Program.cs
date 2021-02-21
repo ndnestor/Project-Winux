c4c51b64d79a304ca7f2620f5e2cab1e42aa667e
YBtheS <449000p@gmail.com>
2021-02-20 22:35:57 -0500

    first commit

---
+++ Winux Parent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Winux_Parent {
+    class Program {
+
+        //private static Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+
+        static void Main(string[] args) {
+            //StripWindows();
+
+            Console.ReadLine();
+        }
+
+        //Remove windows features
+        /*private static void StripWindows() {
+            //Kill Windows Explorer >:)
+            if(explorerProcesses.Length == 1) {
+                explorerProcesses[0].Kill();
+                Console.WriteLine("Killed Windows Explorer");
+            } else {
+                Console.WriteLine("Found " + explorerProcesses.Length + " explorer processes");
+                foreach(Process process in Process.GetProcesses()) {
+                    Console.WriteLine(process.ProcessName);
+                }
+            }
+        }*/
+    }
+}
